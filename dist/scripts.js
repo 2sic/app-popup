7432078293e7912f1a5212887bc25db022baf84c
lucatescari <luca@ltescari.ch>
2017-11-17 10:27:38 +0100

    popup ausgelagert in eigenes file

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,25 +1,46 @@
 $(function() {
     /*
+        General Functions
+    */
+    function popupOut() {
+        $(".app-popup").fadeOut();
+        $("body").css("overflow", "auto");
+    }
+
+    /*
+        Administration functions
+    */
+    $("#app-popup-test-popup").click(function() {
+        $(".app-popup.admin").fadeIn();     
+    });
+
+    $("#app-popup-close-menu").click(function() {
+        $(".app-popup-settings").fadeOut();
+        $(".app-popup-settings-small").fadeIn();
+    });
+    $("#app-popup-open-menu").click(function() {
+        $(".app-popup-settings").fadeIn();
+        $(".app-popup-settings-small").fadeOut();
+    });
+
+    /*
         Close functions
     */
 
     // Click in BG
     $(".app-popup .background").click(function() {
-        $(this).fadeOut();
-        $(".app-popup .popup").fadeOut();
+        popupOut();
     });
 
     // Click on close
     $(".app-popup .popup-close").click(function() {
-        $(".app-popup .background").fadeOut();
-        $(".app-popup .popup").fadeOut();
+        popupOut();
     });
 
     // Press ESC on Keyboard
     $(document).keyup(function(e) {
         if (e.keyCode == 27) {
-            $(".app-popup .background").fadeOut();
-            $(".app-popup .popup").fadeOut();
+            popupOut();
        }
    });
 });
