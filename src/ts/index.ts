1b4e52ddfde9836faee51f07d82e0f977e15744d
Maxim Hüsler <maxim.huesler@2sic.com>
2021-09-29 15:25:39 +0200

    removed autopopup for admin

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,15 +1,15 @@
 let Cookies = require('../../node_modules/js-cookie');
 require('../../node_modules/micromodal');
-declare let MicroModal: any; 
+declare let MicroModal: any;
 
 const winAny = (window as any);
 winAny.appPopupMessage3 ??= {};
 winAny.appPopupMessage3.init ??= init;
 winAny.appPopupMessage3.popUpShown = false; // parameter to prevent more than 1 popups showing simultaniously
 
-function init({ moduleId, duration } : { moduleId: string, duration: string }) {
-    MicroModal.init({awaitCloseAnimation: true});
-    if (!Cookies.get(`seenpopup-${moduleId}`) && winAny.appPopupMessage3.popUpShown === false) {
+function init({ moduleId, duration }: { moduleId: string, duration: string }) {
+    MicroModal.init({ awaitCloseAnimation: true });
+    if (!Cookies.get(`seenpopup-${moduleId}`) && winAny.appPopupMessage3.popUpShown === false && !document.body.classList.contains('role-admin')) {
         winAny.appPopupMessage3.popUpShown = true;
         setTimeout(() => {
             MicroModal.show(`modal-js-${moduleId}`)
