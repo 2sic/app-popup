82e56d82c1438412307559bd505caac7ec700441
Maxim Hüsler <maxim.huesler@2sic.com>
2021-10-01 10:07:02 +0200

    improvements and fixed styling bug

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,13 +7,13 @@ winAny.appPopupMessage3 ??= {};
 winAny.appPopupMessage3.init ??= init;
 winAny.appPopupMessage3.popUpShown = false; // parameter to prevent more than 1 popups showing simultaniously
 
-function init({ moduleId, duration }: { moduleId: string, duration: string }) {
+function init({ domId, duration }: { domId: string, duration: string }) {
     MicroModal.init({ awaitCloseAnimation: true });
-    if (!Cookies.get(`seenpopup-${moduleId}`) && winAny.appPopupMessage3.popUpShown === false && !document.body.classList.contains('role-admin')) {
+    if (!Cookies.get(`seenpopup-${domId}`) && winAny.appPopupMessage3.popUpShown === false && !document.body.classList.contains('role-admin')) {
         winAny.appPopupMessage3.popUpShown = true;
         setTimeout(() => {
-            MicroModal.show(`modal-js-${moduleId}`)
-            Cookies.set(`seenpopup-${moduleId}`, true, { expires: duration })
+            MicroModal.show(domId)
+            Cookies.set(`seenpopup-${domId}`, true, { expires: duration })
         }, 5000)
     }
 }
